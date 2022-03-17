5f27aa4e37cc9b28295b0c02a20c5461e757d445
Eriberto Guzman <eriberto.guzman@outlook.com>
2022-03-17 13:48:10 -0400

    Needs request forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::API
     before_action :set_csrf_cookie
     include ActionController::Cookies
-   
+    include ActionController::RequestForgeryProtection
+  
+    protect_from_forgery with: :exception 
     before_action :authorize
 
     def cookie 
@@ -13,7 +15,7 @@ class ApplicationController < ActionController::API
     def set_csrf_cookie
        cookies["CSRF-TOKEN"] = {
             value: form_authenticity_token,
-            domain: 'desolate-citadel-26683.herokuapp.com/'
+            domain: 'https://desolate-citadel-26683.herokuapp.com'
         }
     end
   
