f1efcceb6620ddb31931b360c95b14c1dbc4aaa0
Eriberto Guzman <eriberto.guzman@outlook.com>
2022-03-17 14:35:35 -0400

    updates to skip before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
     include ActionController::Cookies
     include ActionController::RequestForgeryProtection
     protect_from_forgery with: :exception, prepend: true
-    
+    skip_before_action :verify_authenticity_token
     before_action :authorize
 
     before_filter :cor
