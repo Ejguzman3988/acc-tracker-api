dceffffbb9a799f288e03d913fd2a6968334ddea
Eriberto Guzman <eriberto.guzman@outlook.com>
2022-03-17 13:56:40 -0400

    Updates to domain again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::API
     def set_csrf_cookie
        cookies["CSRF-TOKEN"] = {
             value: form_authenticity_token,
-            domain: 'desolate-citadel-26683.herokuapp.com'
+            domain: :all 
         }
     end
   
